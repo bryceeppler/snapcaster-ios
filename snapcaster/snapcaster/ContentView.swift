39088428601a1261e0b4eecd73f074e0bfff54a3
Bryce Eppler <eppler97@gmail.com>
2022-08-27 06:46:14 -0700

    adding swift project

---
+++ snapcaster/snapcaster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  snapcaster
+//
+//  Created by Bryce Eppler on 2022-08-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ snapcaster/snapcaster/snapcasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  snapcasterApp.swift
+//  snapcaster
+//
+//  Created by Bryce Eppler on 2022-08-27.
+//
+
+import SwiftUI
+
+@main
+struct snapcasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
